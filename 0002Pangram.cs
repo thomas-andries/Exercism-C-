06d71b902b12c0faccb1e6ca2c42a1885a1a2fa0
thomas-andries <46894717+thomas-andries@users.noreply.github.com>
2019-03-01 12:24:39 +0100

    Add files via upload

---
+++ 0002Pangram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        bool isPangram = input.ToLower().Where(ch => Char.IsLetter(ch)).GroupBy(ch => ch).Count() == 26;
+        return isPangram;
+    }
+}
