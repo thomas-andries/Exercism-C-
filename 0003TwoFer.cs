f9586b6a2e78a51ae8fa73875f926d465e9ecb01
thomas-andries <46894717+thomas-andries@users.noreply.github.com>
2019-03-01 12:36:23 +0100

    Add files via upload

---
+++ 0003TwoFer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+  public static class TwoFer
+  {
+      public static string Name(string input = null)
+      {
+          if (input == null) 
+          {
+              return "One for you, one for me.";
+          }
+          else
+          {
+              return "One for " + input + ", one for me.";
+          }
+      }
+  }
